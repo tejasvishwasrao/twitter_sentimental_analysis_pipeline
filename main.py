3b62fff9294a6888059812de7e53a870d6222c85
Tejas Vishwasrao <69673830+tejasvishwasrao@users.noreply.github.com>
2022-03-13 19:19:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ access_token_secret = config['twitter']['access_token_secret']
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-api = tweepy.API(auth, wait_on_rate_limit=True)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=False, compression=True)
 
 public_tweets = api.home_timeline()
 
